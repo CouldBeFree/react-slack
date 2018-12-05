90cc8796732bbb60885810192a48a1b3082a7e6f
Sasha Zoria <alex.zoria@n3wnormal.com>
2018-12-05 11:38:30 +0200

    created register form

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app'
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
+
+var config = {
+    apiKey: "AIzaSyAG1pggMaU3AqwBXd8CZbYxE52x1cS2jm0",
+    authDomain: "slack-clone-64501.firebaseapp.com",
+    databaseURL: "https://slack-clone-64501.firebaseio.com",
+    projectId: "slack-clone-64501",
+    storageBucket: "slack-clone-64501.appspot.com",
+    messagingSenderId: "618453639413"
+};
+firebase.initializeApp(config);
+
+export default firebase;
