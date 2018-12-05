95c47bef9b3cedea8e7fdbf828df54cd90e5907f
Explorer <propokerstar90@gmail.com>
2018-12-05 22:49:36 +0200

    Enabled setting user to the database

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -2,6 +2,7 @@ import React from 'react';
 import firebase from '../../firebase';
 import { Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import md5 from 'md5';
 
 class Register extends React.Component {
     state = {
@@ -10,7 +11,8 @@ class Register extends React.Component {
         password: '',
         passwordConfirmation: '',
         errors: [],
-        loading: false
+        loading: false,
+        usersRef: firebase.database().ref('users')
     };
 
     handleChange = (event) => {
@@ -55,21 +57,36 @@ class Register extends React.Component {
         return !username.length || !email.length || !password.length || !passwordConfirmation.length
     };
 
+    handleInputError = (errors, inputName) => {
+        return errors.some(error =>
+            error.message.toLowerCase().includes(inputName)) ? 'error' : ''
+    };
+
     handleSubmit = (event) => {
         event.preventDefault();
         if(this.isFormValid()){
             this.setState({
-               errors: [],
-               loading: true
+                errors: [],
+                loading: true
             });
             firebase
                 .auth()
                 .createUserWithEmailAndPassword(this.state.email, this.state.password)
                 .then(createdUser => {
                     console.log(createdUser);
-                    this.setState({
-                        loading: false
+                    createdUser.user.updateProfile({
+                        displayName: this.state.username,
+                        photoURL: `http://gravatar.com/avatar/${md5(createdUser.user.email)}?d=identicon`
                     })
+                        .then(() => {
+                            this.saveUser(createdUser).then(() => {
+                                console.log('user saved');
+                            })
+                        })
+                        .catch(err => {
+                            console.log(err);
+                            this.setState({errors: this.state.errors.concat(err), loading:false})
+                        })
                 })
                 .catch(err => {
                     console.error(err);
@@ -79,6 +96,13 @@ class Register extends React.Component {
                     })
                 })
         }
+    };
+
+    saveUser = createdUser => {
+        return this.state.usersRef.child(createdUser.user.uid).set({
+            name: createdUser.user.displayName,
+            avatar: createdUser.user.photoURL
+        });
     };
 
     render(){
@@ -97,13 +121,13 @@ class Register extends React.Component {
                                         onChange={this.handleChange} value={username} type="text"/>
 
                             <Form.Input fluid name="email" icon="mail" iconPosition="left" placeholder="Email Address"
-                                        onChange={this.handleChange} value={email} type="email"/>
+                                        onChange={this.handleChange} value={email} className={this.handleInputError(errors, 'email')} type="email"/>
 
                             <Form.Input fluid name="password" icon="lock" iconPosition="left" placeholder="Password"
-                                        onChange={this.handleChange} value={password} type="password"/>
+                                        onChange={this.handleChange} value={password} className={this.handleInputError(errors, 'password')} type="password"/>
 
                             <Form.Input fluid name="passwordConfirmation" icon="repeat" iconPosition="left"
-                                        placeholder="Password Confirmation" value={passwordConfirmation} onChange={this.handleChange} type="password"/>
+                                        placeholder="Password Confirmation" value={passwordConfirmation} className={this.handleInputError(errors, 'password')} onChange={this.handleChange} type="password"/>
 
                             <Button disabled={loading} className={loading ? 'loading' : ''} color="orange" fluid size="large">Submit</Button>
                         </Segment>
