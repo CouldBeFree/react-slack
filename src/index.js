4eb9f13a279bc38b03ca0964a6fe3301a6cb5b16
Sasha Zoria <alex.zoria@n3wnormal.com>
2018-12-07 18:00:02 +0200

    Made spinner

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import Login from './components/Auth/Login'
+import Spinner from './Spinner'
 import Register from './components/Auth/Register'
 import registerServiceWorker from './registerServiceWorker';
 import firebase from './firebase';
@@ -21,6 +22,7 @@ const store = createStore(rootReducer, composeWithDevTools());
 class Root extends React.Component {
 
     componentDidMount(){
+        console.log(this.props.isLoading);
         firebase.auth().onAuthStateChanged(user => {
             if(user) {
                 this.props.setUser(user);
@@ -30,7 +32,7 @@ class Root extends React.Component {
     }
 
     render(){
-        return (
+        return this.props.isLoading ? <Spinner/> : (
             <Switch>
                 <Route exact path="/" component={App}/>
                 <Route path="/login" component={Login}/>
@@ -40,7 +42,15 @@ class Root extends React.Component {
     }
 }
 
-const RootWithAuth = withRouter(connect(null, { setUser })(Root));
+const mapStateFromProps = state => ({
+    isLoading: state.user.isLoading
+});
+
+const RootWithAuth = withRouter(
+    connect(
+        mapStateFromProps,
+        {setUser}
+        )(Root));
 
 ReactDOM.render(
     <Provider store={store}>
