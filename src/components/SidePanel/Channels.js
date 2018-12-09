618f6560645ea80a38d3faa63da4a82c39c930ca
Explorer <propokerstar90@gmail.com>
2018-12-09 19:07:31 +0200

    Redux

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Menu, Icon, Modal, Form, Input, Button } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { setCurrentChannel } from "../../actions";
 import firebase from '../../firebase';
 
 class Channels extends React.Component{
@@ -24,12 +26,16 @@ class Channels extends React.Component{
         })
     };
 
+    changeChannel = channel => {
+        this.props.setCurrentChannel(channel)
+    };
+
     displayChannels = channels => (
         channels.length > 0 &&
         channels.map(channel => (
             <Menu.Item
                 key={channel.id}
-                onClick={() => console.log(channel)}
+                onClick={() => this.changeChannel(channel)}
                 name={channel.name}
                 style={{ opacity: 0.7 }}
             >
@@ -142,4 +148,4 @@ class Channels extends React.Component{
     }
 }
 
-export default Channels;
+export default connect(null, { setCurrentChannel })(Channels);
