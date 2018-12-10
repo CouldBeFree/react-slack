47b13e30b2c493bc0f3a8e0dad2f1ecfa3daacf7
Sasha Zoria <alex.zoria@n3wnormal.com>
2018-12-10 15:18:31 +0200

    Enabled adding file

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -70,6 +70,10 @@ class MessageForm extends React.Component {
         }
     };
 
+    uploadFile = (file, metadata) => {
+        console.log(file, metadata)
+    };
+
     render(){
         const { errors, message, loading, modal } = this.state;
 
@@ -107,6 +111,7 @@ class MessageForm extends React.Component {
                     <FileModal
                         modal={modal}
                         closeModal={this.closeModal}
+                        uploadFile={this.uploadFile}
                     />
                 </Button.Group>
             </Segment>
